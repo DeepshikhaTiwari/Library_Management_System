309b043b2ec4e8078cd720a3450d6bed0ca7fc45
Deepshikha Tiwari <shikhablb1@gmail.com>
2021-04-05 10:46:48 +0530

    Add path for catalog app

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,6 @@
-from django.urls import path, include
-from django.views.generic import RedirectView
-
-from . import views, admin
+from django.urls import path
+from . import views
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
 ]
