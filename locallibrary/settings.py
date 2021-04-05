d813a9cc32039cdf6b9be3a436e4d1bb0da9ce71
Deepshikha Tiwari <shikhablb1@gmail.com>
2021-04-05 17:21:06 +0530

    Update the TEMPLATES section's 'DIRS'

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'locallibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,9 +116,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+SESSION_SAVE_EVERY_REQUEST = True
+
+LOGIN_REDIRECT_URL = '/'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
