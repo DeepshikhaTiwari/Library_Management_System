620c0da2d696b5fa36f244733da6e6558d41934e
Deepshikha Tiwari <shikhablb1@gmail.com>
2021-04-05 15:15:50 +0530

    Create Book detail view class and function

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
 from catalog.models import Book, Author, BookInstance, Genre
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
@@ -15,3 +17,29 @@ def index(request):
         'num_authors': num_authors,
     }
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    content_object_name = 'my_book_list'
+    queryset = Book.objects.filter(title__icontains='war')[:5]
+    template_name = 'books/my_arbitrary_template_name_list.html'
+
+    def get_queryset(self):
+        return Book.objects.filter(title__icontains='war')[:5]
+
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+    def book_detail_view(request, primary_key):
+        try:
+            book = get_object_or_404(Book, pk=primary_key)
+        except Book.DoesNotExist:
+            return render(request, 'catalog/book_detail.html', context={'book': book})
+
