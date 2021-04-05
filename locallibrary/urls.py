ad72b0de6db1e23b8eb3dbf1261c3a3d56559dbf
Deepshikha Tiwari <shikhablb1@gmail.com>
2021-04-05 10:58:38 +0530

    Add path for admin and catalog app

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -23,8 +23,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='catalog/', permanent=True)),
-    path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('catalog/', include('catalog.urls')),
+                  path('', RedirectView.as_view(url='catalog/', permanent=True)),
+                  path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
