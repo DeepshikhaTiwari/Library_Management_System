d3b6bdde8d02a460578abebf8a03106b66f484f3
Deepshikha Tiwari <shikhablb1@gmail.com>
2021-04-20 18:23:23 +0530

    Change setting.py to deploy in heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
